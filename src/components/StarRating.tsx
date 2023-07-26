872ef188472f87083bb3c13df1f2b94932e59215
Abdul Rehman <r3hmantom@gmail.com>
2023-07-26 19:39:30 +0500

    Hover Star Rating

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -20,27 +20,35 @@ const starStyle = {
 
 const StarRating = ({ maxRating = 5 }) => {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
   return (
     <div style={containerStyle}>
       <div style={startContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => {
           return (
             <Star
-              full={rating >= i + 1}
+              full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
               key={i}
               onRate={() => setRating(i + 1)}
+              onHoverIn={() => setTempRating(i + 1)}
+              onHoverOut={() => setTempRating(0)}
             />
           );
         })}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span onClick={onRate} style={starStyle}>
+    <span
+      onClick={onRate}
+      style={starStyle}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
