46a693cfb1f18352650bb085dc5f375d688459f2
Abdul Rehman <r3hmantom@gmail.com>
2023-08-05 12:15:52 +0500

    Focus search on pressing Enter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface SearchProps {
   query: string;
@@ -6,8 +6,18 @@ interface SearchProps {
 }
 
 const Search: React.FC<SearchProps> = ({ query, setQuery }) => {
+  const inputEl = useRef<HTMLInputElement | null>(null);
+  useEffect(() => {
+    function callback(e: { code: string }) {
+      if (e.code === "Enter") inputEl.current?.focus();
+    }
+    document.addEventListener("keydown", callback);
+
+    return () => document.removeEventListener("keydown", callback);
+  }, []);
   return (
     <input
+      ref={inputEl}
       className="search"
       type="text"
       placeholder="Search movies..."
